d05a2a9ba054a547407dc7e1264e5777f909a164
KALIDBEYAN <86565904+KALIDBEYAN@users.noreply.github.com>
2026-03-14 01:56:57 +0300

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(helmet({
 app.use(cookieParser());
 
 app.use(cors({ 
-  origin: ["https://akaki-frontend.onrender.com", "http://localhost:5173"], // የሬንደር እና የሎካል ሊንክ
+  origin:"https://akaki-frontend.onrender.com"
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
