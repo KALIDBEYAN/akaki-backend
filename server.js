4a6a26eb454be1c0d35ff1f4f828cef36440e452
KALIDBEYAN <86565904+KALIDBEYAN@users.noreply.github.com>
2026-03-14 00:00:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(helmet({
 app.use(cookieParser());
 
 app.use(cors({ 
-  origin: "true", 
+  origin: true, 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
@@ -58,6 +58,8 @@ app.use((err, req, res, next) => {
     message: err.message || 'የውስጥ ሰርቨር ስህተት አጋጥሟል'
   });
 });
-
+app.get("/hello", (req, res) => {
+  res.json({ message: "ሰላም! ሰርቨሩ በትክክል እየሰራ ነው" });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));  
