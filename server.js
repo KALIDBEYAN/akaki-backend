4ad60cdf080fea5c4cfcefc7e893eddc57f5d16a
KALIDBEYAN <86565904+KALIDBEYAN@users.noreply.github.com>
2026-03-14 01:14:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(helmet({
 app.use(cookieParser());
 
 app.use(cors({ 
-  origin:origin:true, 
+  origin:true, 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
