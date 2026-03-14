8d8b479d15974ab121ecb74ac2c6f81ba6655c54
KALIDBEYAN <86565904+KALIDBEYAN@users.noreply.github.com>
2026-03-14 05:05:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ app.use(helmet({
 app.use(cookieParser());
 
 app.use(cors({ 
-  origin:"akaki-frontend.vercel.app",
+  // እዚህ ጋር የ Vercel ሊንክህን ወይም ሁሉንም ለመፍቀድ "*" መጠቀም ትችላለህ
+  origin: process.env.FRONTEND_URL || "http://localhost:5173", 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
